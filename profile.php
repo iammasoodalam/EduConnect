c12eead6642b90ece702ab7c3265625fa0dd77dc
iammasoodalam <130masoodalam@gmail.com>
2021-04-11 22:49:32 +0530

    professor profile is completed.

--- profile.php
+++ profile.php
@@ -3,13 +3,16 @@ if (empty($_SESSION)) {
   header("Location: index.php");
   exit();
 }
-$BranchSql = "SELECT `branchName` FROM `branch` WHERE `branchId` = ?";
-$branchStmt = $mysqli->prepare($BranchSql);
-$branchStmt->bind_param("i", $_SESSION['branch']);
-$branchStmt->execute();
-$branchResult = $branchStmt->get_result();
-$branchRow = $branchResult->fetch_array();
-$_SESSION['branchName'] = $branchRow[0];
+if ($_SESSION['userType'] == 'student'){
+  $BranchSql = "SELECT `branchName` FROM `branch` WHERE `branchId` = ?";
+  $branchStmt = $mysqli->prepare($BranchSql);
+  $branchStmt->bind_param("i", $_SESSION['branch']);
+  $branchStmt->execute();
+  $branchResult = $branchStmt->get_result();
+  $branchRow = $branchResult->fetch_array();
+  $_SESSION['branchName'] = $branchRow[0];
+}
+
 ?>
 <main id="profile">
   <?php include "./includes/schedule-section.inc.php" ?>
@@ -22,21 +25,35 @@ $_SESSION['branchName'] = $branchRow[0];
     <?php }
     ?>
     <ul class="profile-data">
-      <li><b>Name: </b><div><?= $_SESSION['name'] ?></div></li>
-      <li><b>Enrollment No: </b><div><?= $_SESSION['enrollment'] ?></div></li>
-      <li><b>Faculty No: </b><div><?= $_SESSION['facultyNo'] ?></div></li>
-      <li><b>Father's Name: </b><div><?= $_SESSION['fathersName'] ?></div></li>
-      <li><b>Phone No.: </b><?php
-        for($i = 1; $i <= $_SESSION['phoneCount']; $i++){
-          echo "<div>". $_SESSION['phone' . $i] . "</div>";
-        }
-      ?></li>
-      <li><b>Email Id: </b><div><?= $_SESSION['emailId'] ?></div></li>
-      <li><b>Date of Birth: </b><div><?= $_SESSION['dateOfBirth'] ?></div></li>
-      <li><b>Address: </b><div><?= $_SESSION['address'] ?></div></li>
-      <li><b>Join date: </b><div><?= $_SESSION['joinDate'] ?></div></li>
-      <li><b>Branch: </b><div><?= $_SESSION['branchName'] ?></div></li>
-      <li><b>Semester: </b><div><?= $_SESSION['semester'] ?></div></li>
+      <?php if ($_SESSION['userType'] == 'student'){ ?>
+        <li><b>Name: </b><div><?= $_SESSION['name'] ?></div></li>
+        <li><b>Enrollment No: </b><div><?= $_SESSION['enrollment'] ?></div></li>
+        <li><b>Faculty No: </b><div><?= $_SESSION['facultyNo'] ?></div></li>
+        <li><b>Father's Name: </b><div><?= $_SESSION['fathersName'] ?></div></li>
+        <li><b>Phone No.: </b><?php
+          for($i = 1; $i <= $_SESSION['phoneCount']; $i++){
+            echo "<div>". $_SESSION['phone' . $i] . "</div>";
+          }
+        ?></li>
+        <li><b>Email Id: </b><div><?= $_SESSION['emailId'] ?></div></li>
+        <li><b>Date of Birth: </b><div><?= $_SESSION['dateOfBirth'] ?></div></li>
+        <li><b>Address: </b><div><?= $_SESSION['address'] ?></div></li>
+        <li><b>Join date: </b><div><?= $_SESSION['joindate'] ?></div></li>
+        <li><b>Branch: </b><div><?= $_SESSION['branchName'] ?></div></li>
+        <li><b>Semester: </b><div><?= $_SESSION['semester'] ?></div></li>
+      <?php } else { ?>
+        <li><b>Name: </b><div><?= $_SESSION['name'] ?></div></li>
+        <li><b>Professor ID: </b><div><?= $_SESSION['professorId'] ?></div></li>
+        <li><b>Qualification: </b><div><?= $_SESSION['qualification'] ?></div></li>
+        <li><b>Phone No.: </b><?php
+          for($i = 1; $i <= $_SESSION['phoneCount']; $i++){
+            echo "<div>". $_SESSION['phone' . $i] . "</div>";
+          }
+        ?></li>
+        <li><b>Email Id: </b><div><?= $_SESSION['emailId'] ?></div></li>
+        <li><b>Address: </b><div><?= $_SESSION['address'] ?></div></li>
+        <li><b>Join date: </b><div><?= $_SESSION['joindate'] ?></div></li>
+      <?php } ?>
     </ul>
   </div>
 </main>
