0bc193118eb11bc74f8316cae4c022c059a09ba0
iammasoodalam <130masoodalam@gmail.com>
2021-04-25 02:00:03 +0530

    Home section completed

--- home.php
+++ home.php
@@ -111,7 +111,97 @@ if($_SESSION['userType'] == "admin"){
   } else {
     ?>
     <div id="home-professor">
-    <h2>Welcome <?= $_SESSION['name'] ?></h2>
+      <div id="assignments">
+        <h2>Assignments</h2>
+        <?php
+          $sqlAssignement = "SELECT * FROM assignment WHERE providedBy = ? ORDER BY provideddate DESC";
+          $stmtAssignment = $mysqli->prepare($sqlAssignement);
+          $stmtAssignment->bind_param("s", $_SESSION['professorId']);
+          $stmtAssignment->execute();
+          if($result = $stmtAssignment->get_result()){
+            if($result->num_rows > 0){
+              while($row = $result->fetch_assoc()){
+                ?>
+                <div class="card">
+                  <div class="card-head">
+                    <div class="provided">Date: <?= $row['provideddate']; ?></div>
+                    <div class="submission">Submit By: <?= $row['submissiondate'] ?></div>
+                  </div>
+                  <h3><?=$row['coursecode'] . " - " . $row['title'] ?></h3>
+                  <div class="card-data">
+                    <?= $row['data'] ?>
+                    <?php
+                    if($row['attachment'] == 1){
+                      $dir = "./asset/attachments/";
+                      $list = scandir($dir);
+                      $fileName = "/assignment" . $row['assignmentId'] . "/i";
+                      foreach ($list as $value) {
+                        if(preg_match($fileName, $value)){
+                          $fileName = $value;
+                          break;
+                        }
+                      }
+                      ?>
+                      <hr>
+                      <a href="./asset/attachments/<?= $fileName ?>">Attachment</a>
+                      <?php
+                    }
+                    ?>
+                  </div>
+                  <p class="read-more">Read More <i class="fas fa-caret-down"></i></p>
+                </div>
+                <?php
+              }
+            }
+          }
+          
+        ?>
+      </div>
+      <div id="lectureNotes">
+        <h2>Lecture Notes</h2>
+        <?php
+          $sqlLectureNotes = "SELECT * FROM lecturenotes WHERE providedBy = ? ORDER BY provideddate DESC";
+          $stmtLectureNotes = $mysqli->prepare($sqlLectureNotes);
+          $stmtLectureNotes->bind_param("s", $_SESSION['professorId']);
+          $stmtLectureNotes->execute();
+          if($result = $stmtLectureNotes->get_result()){
+            if($result->num_rows > 0){
+              while($row = $result->fetch_assoc()){
+                ?>
+                <div class="card">
+                  <div class="card-head">
+                    <div class="provided">Date: <?= $row['provideddate']; ?></div>
+                  </div>
+                  <h3><?=$row['coursecode'] . " - " . $row['title'] ?></h3>
+                  <div class="card-data">
+                    <?= $row['data'] ?>
+                    <?php
+                    if($row['attachment'] == 1){
+                      $dir = "./asset/attachments/";
+                      $list = scandir($dir);
+                      $fileName = "/lectureNote" . $row['lectureId'] . "/i";
+                      foreach ($list as $value) {
+                        if(preg_match($fileName, $value)){
+                          $fileName = $value;
+                          break;
+                        }
+                      }
+                      ?>
+                      <hr>
+                      <a href="./asset/attachments/<?= $fileName ?>">Attachment</a>
+                      <?php
+                    }
+                    ?>
+                  </div>
+                  <p class="read-more">Read More <i class="fas fa-caret-down"></i></p>
+                </div>
+                <?php
+              }
+            }
+          }
+          
+        ?>
+      </div>
     </div>
     <?php
   }
