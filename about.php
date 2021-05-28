5f3a2aea1004dd257f57ff41419d850868bdad5f
iammasoodalam <130masoodalam@gmail.com>
2021-05-28 17:46:28 +0530

    Nothing changes in this page

--- about.php
+++ about.php
@@ -1,4 +1,10 @@
 <?php require_once "./includes/header.inc.php"; ?>
+<!-- <main id="aboutSection">
+  <div class="container">
+    <img src="./asset/admin.png" alt="Image of Mohd Masood Alam" />
+    <div class="adminName">Mohd Masood Alam</div>
 
+  </div>
+</main> -->
 
 <?php require_once "./includes/footer.inc.php"; ?>
