8486fd8f965afc9443e65d6eaca22ea95828c985
iammasoodalam <130masoodalam@gmail.com>
2021-06-03 11:48:50 +0530

    Added jquery as a link to cdn and removed files

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -5,8 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
-  <script src="./js/jQuery.js"></script>
-  <script src="./js/popper.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <link rel="shortcut icon" href="favicon.svg" type="image/x-icon">
   <link rel="stylesheet" href="./css/style.css?v=<?php echo time(); ?>">
   <title>EduConnect - A Single learning plateform to connect student and teachers directly</title>
